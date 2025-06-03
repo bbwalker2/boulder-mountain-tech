fb439124e93ab0422a5c5ab41071689c9f45ca88
bbwalker2 <bbwalker2@gmail.com>
2025-06-03 19:51:57 +0700

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>Welcome to Boulder Mountain Tech</h1>
-      <p>Empowering Innovation. Delivering Results.</p>
-    </div>
-  );
-}
